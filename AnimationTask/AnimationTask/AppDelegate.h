f67cae05dd4e5534c7d1d7c9b4776d14e1e6b2f3
HomeStation <homestation@HomeStations-Mac.local>
2017-08-18 17:31:39 -0700

    Animations added using UIView animation block and UIDynamicAnimator

---
+++ AnimationTask/AnimationTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTask
+//
+//  Created by HomeStation on 8/16/17.
+//  Copyright © 2017 AKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
